0f2f4e4889710d662976c074f24d40fc3e202bb7
wanghao <wh_199404@163.com>
2021-10-10 15:51:24 +0800

    NioServer

---
+++ src/main/java/com/example/net/nio/NioServer.java
@@ -0,0 +1,96 @@
+package com.example.net.nio;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+
+import static com.example.net.utils.ByteBufferUtil.debugAll;
+
+@Slf4j
+public class NioServer {
+
+    public void bind(int port) throws IOException {
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.configureBlocking(false);
+        // 1. 创建 selector, 管理多个 channel
+        Selector selector = Selector.open();
+        // 2. 建立 selector 和 channel 的联系（注册）
+        // SelectionKey 就是将来事件发生后，通过它可以知道事件和哪个channel的事件
+        // key 只关注 accept 事件
+        SelectionKey sscKey = ssc.register(selector, SelectionKey.OP_ACCEPT);
+        log.debug("sscKey:{}", sscKey);
+        ssc.bind(new InetSocketAddress(port));
+        while (true) {
+            // 3. select 方法, 没有事件发生，线程阻塞，有事件，线程才会恢复运行
+            // select 在事件未处理时，它不会阻塞, 事件发生后要么处理，要么取消，不能置之不理
+            selector.select();
+            // 4. 处理事件, selectedKeys 内部包含了所有发生的事件
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                // 处理key 时，要从 selectedKeys 集合中删除，否则下次处理就会有问题
+                iterator.remove();
+                // 5. 区分事件类型
+                if (key.isAcceptable()) {  // 如果是 accept
+                    SocketChannel sc = (SocketChannel) key.channel();
+                    sc.configureBlocking(false);
+                    ByteBuffer buffer = ByteBuffer.allocate(16); // attachment
+                    // 将一个 byteBuffer 作为附件关联到 selectionKey 上
+                    SelectionKey scKey = sc.register(selector, SelectionKey.OP_READ, buffer);
+                    log.debug("连接已建立: {}", sc);
+                    log.debug("scKey:{}", scKey);
+                } else if (key.isReadable()) { // 如果是 read
+                    try {
+                        // 拿到触发事件的channel
+                        SocketChannel channel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = (ByteBuffer) key.attachment();
+                        int read = channel.read(buffer);
+                        // 如果是正常断开，read 的方法的返回值是 -1
+                        if (read == -1) {
+                            key.cancel();
+                        } else {
+                            split(buffer);
+                            // 是否需要扩容
+                            if (buffer.position() == buffer.limit()) {
+                                ByteBuffer newBuffer = ByteBuffer.allocate(buffer.capacity() * 2);
+                                buffer.flip();
+                                newBuffer.put(buffer);
+                                key.attach(newBuffer);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        // 因为客户端断开了,因此需要将 key 取消（从 selector 的 keys 集合中真正删除 key）
+                        key.cancel();
+                    }
+                }
+            }
+        }
+    }
+
+    private static void split(ByteBuffer source) {
+        source.flip();
+        for (int i = 0; i < source.limit(); i++) {
+            // 找到一条完整消息
+            if (source.get(i) == '\n') {
+                int length = i + 1 - source.position();
+                // 把这条完整消息存入新的 ByteBuffer
+                ByteBuffer target = ByteBuffer.allocate(length);
+                // 从 source 读，向 target 写
+                for (int j = 0; j < length; j++) {
+                    target.put(source.get());
+                }
+                debugAll(target);
+            }
+        }
+        source.compact(); // 0123456789abcdef  position 16 limit 16
+    }
+
+    public static void main(String[] args) throws IOException {
+        new NioServer().bind(8080);
+    }
+}
--- src/main/java/com/example/net/nio/NioTimeClient.java
+++
@@ -1,7 +0,0 @@
-package com.example.net.nio;
-
-public class NioTimeClient {
-    public static void main(String[] args) {
-        new Thread(new TimeClientHandler("127.0.0.1", 8080)).start();
-    }
-}
