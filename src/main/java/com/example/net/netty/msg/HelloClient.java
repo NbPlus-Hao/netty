4162af6bae38df6938204dfdc8b4ccf3ca731c5b
wanghao <wh_199404@163.com>
2021-10-20 20:46:24 +0800

    netty-短连接方式解决粘包

--- src/main/java/com/example/net/netty/msg/HelloClient.java
+++ src/main/java/com/example/net/netty/msg/HelloClient.java
@@ -15,6 +15,13 @@ import lombok.extern.slf4j.Slf4j;
 public class HelloClient {
 
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            send();
+        }
+        log.info("finish");
+    }
+
+    private static void send() {
         NioEventLoopGroup worker = new NioEventLoopGroup();
         try {
             Bootstrap bootstrap = new Bootstrap();
@@ -26,11 +33,10 @@ public class HelloClient {
                             ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
                                 @Override
                                 public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                                    for (int i = 0; i < 10; i++) {
-                                        ByteBuf buf = ctx.alloc().buffer();
-                                        buf.writeBytes(new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
-                                        ctx.writeAndFlush(buf);
-                                    }
+                                    ByteBuf buf = ctx.alloc().buffer();
+                                    buf.writeBytes(new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
+                                    ctx.writeAndFlush(buf);
+                                    ctx.close();
                                 }
                             });
                         }
